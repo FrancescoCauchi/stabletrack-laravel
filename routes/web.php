1fd02e7dd703f547753fec24db618e1393c67596
Francesco Cauchi <francesco.cauchi.j95523@mcast.edu.mt>
2026-01-06 10:22:34 +0000

    add statuses resource routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StableController;
+use App\Http\Controllers\HorseController;
+use App\Http\Controllers\HorseStatusController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('stables.index');
 });
 
 Route::resource('stables', StableController::class);
+Route::resource('horses', HorseController::class);
+Route::resource('statuses', HorseStatusController::class)->except(['show']);
